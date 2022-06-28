d0d32a49283eda90fc3ed7ca39da84017d08dda9
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-06-28 10:50:53 +0300

    updated: some files

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,18 @@
 import express from "express";
 
+// Routes
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouters } from "./routes/signup";
+
 const app = express();
 app.use(express.json());
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi there!");
-});
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signupRouters);
+app.use(signoutRouter);
 
 app.listen(3000, () => {
   console.log("Server Listening on port 3000!");
