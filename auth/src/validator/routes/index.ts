f3eda65166f24f0e23c44e925bed49126d73344a
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-06-28 10:50:42 +0300

    added: validator in routes

---
+++ auth/src/validator/routes/index.ts
@@ -0,0 +1,12 @@
+import { body } from "express-validator";
+
+export const signupValidator = [
+  body("email").isEmail().withMessage("Email must be verified!."),
+  body("password")
+    .trim()
+    .isLength({
+      min: 4,
+      max: 20,
+    })
+    .withMessage("Password must be between 4 and 20 characters!."),
+];
