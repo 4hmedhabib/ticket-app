15a8528389f871ea0188aff06ac6bb6a34e15ea9
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-06-29 17:17:58 +0300

    updated: auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,8 @@
 import express from "express";
+import "express-async-errors";
+
+// Mongoose
+import mongoose from "mongoose";
 
 // Routes
 import { currentUserRouter } from "./routes/current-user";
@@ -8,6 +12,7 @@ import { signupRouters } from "./routes/signup";
 
 //Middleware
 import { errorHandler } from "./middleware/error-handler";
+import { NotFoundError } from "./errors/not-found";
 
 const app = express();
 app.use(express.json());
@@ -17,8 +22,24 @@ app.use(signinRouter);
 app.use(signupRouters);
 app.use(signoutRouter);
 
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Server Listening on port 3000!");
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+
+    console.log("CONNECTED TO MONGODB SUCCESSFULLY");
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log("Server Listening on port 3000!");
+  });
+};
+
+start();
