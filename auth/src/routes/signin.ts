2c92f3cddb44e49555a668b2ac959e8f67d7f327
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-07-02 10:14:02 +0300

    updated: signin route in routes

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,15 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
 
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {
-  res.send("Hi There current-user");
+router.post("/api/users/signin", (req: Request, res: Response) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    throw new RequestValidationError(errors.array());
+  }
 });
 
 export { router as signinRouter };
