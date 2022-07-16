57430a59799dddde9f6fbb04378ce8fdb7b1d157
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-07-16 17:32:36 +0300

    updated: added require auth

---
+++ auth/src/middlewares/requireAuth.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Request, Response } from "express";
+
+export const requireAuth = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.currentUser) {
+    return res.status(401).send();
+  }
+};
